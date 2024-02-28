711254417f83952c05d881ee9a21ff88e9b02a42
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-28 18:16:03 +0000

    paypal donate button

---
+++ app/components/PayPalDonate.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button, IconButton } from "@mui/material";
+import PaidIcon from "@mui/icons-material/Paid";
+
+const PayPalDonate = () => {
+  return (
+    <Button
+      endIcon={<PaidIcon />}
+      variant="outlined"
+      sx={{ borderRadius: "20px", backgroundColor: "#339465", float: "right" }}
+    >
+      Donate
+    </Button>
+  );
+};
+
+export default PayPalDonate;
