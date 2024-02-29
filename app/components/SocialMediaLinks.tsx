68d79053dd189a2bba2745b6f4868b9f6e85cadc
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-29 14:52:32 +0000

    created social media links for footer

---
+++ app/components/SocialMediaLinks.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Icon, IconButton, Stack } from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import XIcon from "@mui/icons-material/X";
+import Link from "next/link";
+const SocialMediaLinks = () => {
+  return (
+    <Stack
+      direction={"row"}
+      spacing={2}
+      alignItems={"center"}
+      justifyContent={"center"}
+      marginBottom={5}
+    >
+      <Link href={"https://www.facebook.com/KentFilmFoundation/"}>
+        <IconButton sx={{ backgroundColor: "#339465", color: "white" }}>
+          <FacebookIcon />
+        </IconButton>
+      </Link>
+      <Link href={"https://www.instagram.com/ramsgatecinema/"}>
+        <IconButton sx={{ backgroundColor: "#339465", color: "white" }}>
+          <InstagramIcon />
+        </IconButton>
+      </Link>
+      <Link href={"https://twitter.com/kentfilm_uk?lang=en-GB"}>
+        <IconButton sx={{ backgroundColor: "#339465", color: "white" }}>
+          <XIcon />
+        </IconButton>
+      </Link>
+    </Stack>
+  );
+};
+
+export default SocialMediaLinks;
