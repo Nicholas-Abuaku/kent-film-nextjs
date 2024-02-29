4156111295f82de99a04162771ad164be96c61b1
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-29 21:17:03 +0000

    Button now routes to /dashboard/press

--- app/components/DashboardManageNewsButton.tsx
+++ app/components/DashboardManageNewsButton.tsx
@@ -9,33 +9,32 @@ import {
   CardActionArea,
 } from "@mui/material";
 import ManageHistoryIcon from "@mui/icons-material/ManageHistory";
+import Link from "next/link";
 
 const DashboardManageNewsButton = () => {
   return (
-    <CardActionArea
-      sx={{ width: "300px", height: "140px" }}
-      //   component={Link}
-      //   to={"/dashboard/edit-latest-screening"}
-    >
-      <Card
-        sx={{
-          width: "300px",
-          height: "140px",
-          background:
-            "linear-gradient(90deg, rgba(0,36,4,1) 0%, rgba(43,125,74,1) 50%)",
-          color: "white",
-        }}
-      >
-        <CardContent>
-          <Stack direction={"row"} spacing={1}>
-            <Typography variant="h4">Manage Press Articles</Typography>
-            <Icon sx={{ height: "40px" }}>
-              <ManageHistoryIcon />
-            </Icon>
-          </Stack>
-        </CardContent>
-      </Card>
-    </CardActionArea>
+    <Link href={"/dashboard/press"}>
+      <CardActionArea sx={{ width: "300px", height: "140px" }}>
+        <Card
+          sx={{
+            width: "300px",
+            height: "140px",
+            background:
+              "linear-gradient(90deg, rgba(0,36,4,1) 0%, rgba(43,125,74,1) 50%)",
+            color: "white",
+          }}
+        >
+          <CardContent>
+            <Stack direction={"row"} spacing={1}>
+              <Typography variant="h4">Manage Press Articles</Typography>
+              <Icon sx={{ height: "40px" }}>
+                <ManageHistoryIcon />
+              </Icon>
+            </Stack>
+          </CardContent>
+        </Card>
+      </CardActionArea>
+    </Link>
   );
 };
 
