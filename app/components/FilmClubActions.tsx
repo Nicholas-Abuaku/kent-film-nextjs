701a5b61a593e63ddec8b19bbaae937374dbee63
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-29 14:52:04 +0000

    created film club actions form

---
+++ app/components/FilmClubActions.tsx
@@ -0,0 +1,70 @@
+"use client";
+import React, { useState, useReducer, useContext } from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import Snackbar from "@mui/material/Snackbar";
+import { ManageTableContext } from "../contexts/ManageTableContext";
+import Link from "next/link";
+import axios from "axios";
+type FilmClubActionsProps = {
+  id: number;
+  onDelete: Function;
+};
+const FilmClubActions = (props: FilmClubActionsProps) => {
+  const { tableUpdate, setTableUpdate } = useContext(ManageTableContext);
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const open = Boolean(anchorEl);
+  const [openSnack, setOpenSnack] = useState();
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    console.log(props.id);
+  };
+
+  const handleDelete = () => {
+    console.log("no");
+  };
+
+  return (
+    <>
+      <Snackbar
+        open={openSnack}
+        autoHideDuration={200000}
+        onClose={() => console.log("Close")}
+        message="Film Club Deleted"
+      />
+      <Button
+        variant="contained"
+        color="warning"
+        id="film-clubs-actions-button"
+        onClick={handleClick}
+        aria-controls={open ? "film-clubs-actions" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        endIcon={<ArrowDropDownIcon />}
+      >
+        Actions
+      </Button>
+      <Menu
+        id="film-clubs-actions"
+        anchorEl={anchorEl}
+        open={open}
+        MenuListProps={{ "aria-labelledby": "film-clubs-actions-button" }}
+        onClose={handleClose}
+      >
+        <Link href={"/dashboard/film-clubs/edit/" + props.id}>
+          <MenuItem onClick={handleClose}>Edit</MenuItem>
+        </Link>
+        <MenuItem onClick={handleDelete}>Delete</MenuItem>
+      </Menu>
+    </>
+  );
+};
+
+export default FilmClubActions;
