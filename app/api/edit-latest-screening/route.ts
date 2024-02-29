cd531ec647e662e36286c326984d90033c6c6403
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-29 14:50:50 +0000

    Created api route to update latest screening

---
+++ app/api/edit-latest-screening/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import axios from "axios";
+
+export async function POST(request: NextRequest, response: NextResponse) {
+  const API_KEY = process.env.VITE_Laravel_API_KEY;
+  const headers = {
+    Authorization: `Bearer ${API_KEY}`,
+    "Content-Type": "multipart/form-data",
+  };
+
+  try {
+    const formData = await request.formData();
+    const title = formData.get("heading");
+    const imgUrl = formData.get("img_Url");
+    // return NextResponse.json({
+    //   heading: title,
+    //   img: imgUrl,
+    // });
+    const axiosRes = await axios.post(
+      "https://kentfilm.up.railway.app/api/featured-content",
+      formData,
+      { headers }
+    );
+    if (axiosRes.status === 201) {
+      return NextResponse.json({ success: axiosRes.data });
+    } else {
+      return NextResponse.json({ error: axiosRes.data });
+    }
+  } catch (err) {
+    return NextResponse.json({ message: err });
+  }
+
+  // return NextResponse.json({message:})
+}
