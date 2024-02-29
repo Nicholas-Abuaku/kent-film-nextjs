2bb9e7bdb5982a21641791d7a44726dabd32571e
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-29 21:19:07 +0000

    Press article table

--- app/dashboard/press/page.tsx
+++ app/dashboard/press/page.tsx
@@ -1,7 +1,37 @@
-import React from "react";
-
+"use client";
+import { IconButton, Stack, Typography, Grid, Button } from "@mui/material";
+import { ManageTableContext } from "@/app/contexts/ManageTableContext";
+import React, { useState } from "react";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import Link from "next/link";
+import PressArticleTable from "@/app/components/PressArticleTable";
+// import { PressArticleTable } from "../../components/back-end/PressArticleTable";
 const page = () => {
-  return <div>Press</div>;
+  const [tableUpdate, setTableUpdate] = useState(false);
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <Stack direction={"row"} spacing={2}>
+          <Link href={"/dashboard"}>
+            <IconButton>
+              <ArrowBackIcon />
+            </IconButton>
+          </Link>
+          <Typography variant="h3">Press Articles</Typography>
+        </Stack>
+        <Link href={"/dashboard/press/new"}>
+          <Button variant="contained" color="success" sx={{ float: "right" }}>
+            Add
+          </Button>
+        </Link>
+      </Grid>
+      <Grid item xs={12}>
+        <ManageTableContext.Provider value={{ tableUpdate, setTableUpdate }}>
+          <PressArticleTable />
+        </ManageTableContext.Provider>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default page;
