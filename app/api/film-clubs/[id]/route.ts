4ee894dfb5b7a93711053109c1a6f70d27ef5eae
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-02-29 21:14:54 +0000

    Film club edit route

---
+++ app/api/film-clubs/[id]/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import axios from "axios";
+import { useParams } from "next/navigation";
+import { NextApiRequest } from "next";
+
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+  response: NextResponse
+) {
+  const API_KEY = process.env.VITE_Laravel_API_KEY;
+  const headers = {
+    Authorization: `Bearer ${API_KEY}`,
+    "Content-Type": "multipart/form-data",
+  };
+  console.log("PARAMS: " + params.id);
+  try {
+    const formData = await request.formData();
+    console.log("formData:", formData); // Log for debugging
+
+    const axiosRes = await axios.post(
+      "https://kentfilm.up.railway.app/api/film-clubs/" + params.id,
+      formData,
+      { headers }
+    );
+
+    if (axiosRes.status === 200) {
+      return NextResponse.json({ success: axiosRes.data });
+    } else {
+      return NextResponse.json({ error: axiosRes.status });
+    }
+  } catch (err) {
+    return NextResponse.json({ message: err });
+  }
+
+  return NextResponse.json({ message: params.id });
+}
