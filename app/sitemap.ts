b90c068dcbf4135ed44137e42bc3417e537b4c2b
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-03-01 16:19:33 +0000

    created sitemap file

---
+++ app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/film-clubs`,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/press`,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/contact`,
+    },
+  ];
+}
