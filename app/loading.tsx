89c3d3be1cd82c8b2cc69b5207b3a8735dc72786
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-03-01 13:27:24 +0000

    Loading file for pages

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const loading = () => {
+  return (
+    <main style={{ textAlign: "center" }}>
+      <Typography variant="h4">Loading...</Typography>
+    </main>
+  );
+};
+
+export default loading;
