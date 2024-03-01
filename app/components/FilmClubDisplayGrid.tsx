878fafd5372a354aba9c80e398d103e8488a7670
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-03-01 13:29:02 +0000

    Film club display grid now makes axios call instead

--- app/components/FilmClubDisplayGrid.tsx
+++ app/components/FilmClubDisplayGrid.tsx
@@ -1,23 +1,38 @@
-//
+"use client";
 import { Grid, Skeleton, Typography } from "@mui/material";
 
 import FilmClubDisplayCard from "./FilmClubDisplayCard";
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 interface FilmClub {
   id: number;
   heading: string;
   description: string;
   img_Url: string;
 }
-const FilmClubDisplayGrid = async () => {
-  const res = await fetch("https://kentfilm.up.railway.app/api/film-clubs");
-  const filmClubs: FilmClub[] = await res.json();
+const FilmClubDisplayGrid = () => {
+  const [filmClubs, setFilmClubs] = useState([]);
+  const fetchFilmClubs = async () => {
+    try {
+      const response = await axios.get(
+        "https://kentfilm.up.railway.app/api/film-clubs"
+      );
+      // console.log(response.data);
+      setFilmClubs(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchFilmClubs();
+  }, []);
 
   //fetch film club data
   return (
     <>
       <Grid container>
-        {filmClubs.map((club) => (
+        {filmClubs.map((club: FilmClub) => (
           <Grid item md={6} xs={11} xl={3} key={club.id}>
             <FilmClubDisplayCard
               heading={club.heading}
