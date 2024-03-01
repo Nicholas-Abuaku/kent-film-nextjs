5a7fc580c777b37b3dcd910b62b45036211500c5
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-03-01 13:28:43 +0000

    Delete route for press article

---
+++ app/api/press/delete/[id]/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import axios from "axios";
+import { useParams } from "next/navigation";
+import { NextApiRequest } from "next";
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+  response: NextResponse
+) {
+  const API_KEY = process.env.VITE_Laravel_API_KEY;
+  const headers = {
+    Authorization: `Bearer ${API_KEY}`,
+    "Content-Type": "multipart/form-data",
+  };
+  console.log("PARAMS: " + params.id);
+  try {
+    console.log("formData:"); // Log for debugging
+
+    const axiosRes = await axios.delete(
+      "https://kentfilm.up.railway.app/api/press/" + params.id,
+
+      { headers }
+    );
+
+    if (axiosRes.status === 200) {
+      return NextResponse.json({ success: axiosRes.data });
+    } else {
+      return NextResponse.json({ error: axiosRes.status });
+    }
+  } catch (err) {
+    return NextResponse.json({ message: err });
+  }
+
+  return NextResponse.json({ message: params.id });
+}
