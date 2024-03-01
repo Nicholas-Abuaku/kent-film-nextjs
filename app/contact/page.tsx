e56141db752ed572d0078e71c1b06a1a70ebc545
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-03-01 16:21:04 +0000

    contact page title

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ContactForm from "../components/ContactForm";
-
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Contact Us",
+};
 function Contact() {
   return <ContactForm />;
 }
