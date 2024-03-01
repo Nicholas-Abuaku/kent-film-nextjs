73b13652e058d1621ffdd3e17c3d4e8619b1eeb7
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-03-01 16:30:57 +0000

    changed redirect url

--- middleware.ts
+++ middleware.ts
@@ -4,10 +4,12 @@ export default function middleware(req: NextRequest) {
   let verify = req.cookies.get("isLoggedIn");
   let url = req.url;
   if (!verify && url.includes("/dashboard")) {
-    return NextResponse.redirect("http://localhost:3000/login");
+    return NextResponse.redirect(`${process.env.NEXT_PUBLIC_BASE_URL}/login`);
   }
 
-  if (verify && url === "http://localhost:3000/login") {
-    return NextResponse.redirect("http://localhost:3000/dashboard");
+  if (verify && url === `${process.env.NEXT_PUBLIC_BASE_URL}/login`) {
+    return NextResponse.redirect(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/dashboard`
+    );
   }
 }
