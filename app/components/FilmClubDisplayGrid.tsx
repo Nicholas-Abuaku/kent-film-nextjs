7dc286157077a97cab42fadd2a12588f98c62c4e
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-03-01 16:20:38 +0000

    isMobile function in here

--- app/components/FilmClubDisplayGrid.tsx
+++ app/components/FilmClubDisplayGrid.tsx
@@ -1,9 +1,19 @@
 "use client";
-import { Grid, Skeleton, Typography } from "@mui/material";
+import {
+  Grid,
+  Skeleton,
+  Typography,
+  useMediaQuery,
+  CardMedia,
+  CardContent,
+  Stack,
+  Card,
+} from "@mui/material";
 
 import FilmClubDisplayCard from "./FilmClubDisplayCard";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import FilmClubTheme from "../Themes/FilmClubTheme";
 interface FilmClub {
   id: number;
   heading: string;
@@ -11,6 +21,7 @@ interface FilmClub {
   img_Url: string;
 }
 const FilmClubDisplayGrid = () => {
+  const isMobile = useMediaQuery(FilmClubTheme.breakpoints.down("md"));
   const [filmClubs, setFilmClubs] = useState([]);
   const fetchFilmClubs = async () => {
     try {
@@ -31,6 +42,80 @@ const FilmClubDisplayGrid = () => {
   //fetch film club data
   return (
     <>
+      {isMobile ? (
+        <Card
+          sx={{
+            backgroundColor: "#127346",
+            width: "90%",
+            height: "100%",
+            margin: "auto",
+            marginTop: "40px",
+            marginBottom: "30px",
+            // width:"64.16vw",
+            // height:"65.18vh"
+          }}
+        >
+          <CardMedia
+            component={"img"}
+            height={"100%"}
+            src="//img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/Girls%20film%20club%201.jpg/:/cr=t:16.67%25,l:0%25,w:100%25,h:66.67%25/rs=w:1240,h:620,cg:true"
+            alt="Free Youth Clubs Every Monday"
+            title="Free Youth Clubs Every Monday"
+          />
+          <CardContent>
+            <Stack spacing={2}>
+              <Typography
+                textAlign={"center"}
+                variant="h6"
+                color={"white"}
+                component={"h2"}
+              >
+                FREE YOUTH FILM CLUBS EVERY MONDAY (term time only) 6-8pm
+              </Typography>
+              <Typography textAlign={"center"} color={"white"}>
+                For more information contact: kentfilmfoundation@gmail.com{" "}
+              </Typography>
+            </Stack>
+          </CardContent>
+        </Card>
+      ) : (
+        <Card
+          // height={"65.18vh"}
+          sx={{
+            backgroundColor: "#127346",
+            width: "60%",
+            margin: "auto",
+            marginTop: "40px",
+            marginBottom: "30px",
+            maxWidth: "80%",
+          }}
+          elevation={24}
+        >
+          <CardMedia
+            component={"img"}
+            height={"50%"}
+            src="//img1.wsimg.com/isteam/ip/c75f83f5-5376-471b-af2d-7c3435beb175/Girls%20film%20club%201.jpg/:/cr=t:16.67%25,l:0%25,w:100%25,h:66.67%25/rs=w:1240,h:620,cg:true"
+            alt="Free Youth Clubs Every Monday"
+            title="Free Youth Clubs Every Monday"
+            loading="eager"
+          />
+          <CardContent>
+            <Stack spacing={2}>
+              <Typography
+                textAlign={"center"}
+                variant="h5"
+                color={"white"}
+                component={"h2"}
+              >
+                FREE YOUTH FILM CLUBS EVERY MONDAY (term time only) 6-8pm
+              </Typography>
+              <Typography textAlign={"center"} color={"white"}>
+                For more information contact: kentfilmfoundation@gmail.com{" "}
+              </Typography>
+            </Stack>
+          </CardContent>
+        </Card>
+      )}
       <Grid container>
         {filmClubs.map((club: FilmClub) => (
           <Grid item md={6} xs={11} xl={3} key={club.id}>
