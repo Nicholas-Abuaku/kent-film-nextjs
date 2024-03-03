3874da8d5389ab002239feb380099ca933b34e4f
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-03-03 12:12:53 +0000

    added more meta data for film clubs

--- app/film-clubs/page.tsx
+++ app/film-clubs/page.tsx
@@ -18,6 +18,25 @@ import FilmClubTheme from "../Themes/FilmClubTheme";
 import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Film Clubs",
+
+  description:
+    "Join Kent Film Foundation's Free Youth Film Clubs at Ramsgate Community Cinema - Mondays, 6-8pm.",
+  openGraph: {
+    title:
+      "Unlock Your Creativity with Kent Film Foundation's Free Youth Film Clubs | Explore Wildlife Filmmaking and More! Mondays, 6-8pm at Ramsgate Community Cinema",
+    description:
+      "Join Kent Film Foundation's Free Youth Film Clubs at Ramsgate Community Cinema - Mondays, 6-8pm.",
+    type: "website",
+    url: "http://localhost:3000/film-clubs",
+  },
+  twitter: {
+    title: "Kent Film Foundation",
+    description:
+      "Explore films at Ramsgate Community Cinema. Check this month's screenings, enjoy events, and connect on social media. Your cinematic journey begins here!",
+  },
+  alternates: {
+    canonical: `https://kentfilmfoundation.netlify.app/film-clubs`,
+  },
 };
 const FilmClubs = () => {
   return (
