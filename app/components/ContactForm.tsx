b6d8a861bf419a26b1cb6052dffd77cde71c09ee
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-03-04 18:58:36 +0000

    Contact form is responsive, no need for  use client

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React from "react";
 import {
   Box,
@@ -14,138 +13,77 @@ import {
 } from "@mui/material";
 import ContactTheme from "../Themes/ContactTheme";
 const ContactForm = () => {
-  //   const ContactTheme = createTheme({});
-  const isMobile = useMediaQuery(ContactTheme.breakpoints.down("md"));
   return (
     <ThemeProvider theme={ContactTheme}>
-      {isMobile ? (
-        <Box width={"100%"}>
-          <form style={{ width: "100%" }}>
-            <Typography
-              textAlign={"left"}
-              variant="h6"
-              component={"h2"}
-              color={"#339465"}
+      <Box
+        width={"50%"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        margin={"auto"}
+        sx={{
+          "@media (max-width: 600px)": {
+            width: "90%",
+          },
+          "@media (max-width: 810px)": {
+            width: "70%",
+          },
+        }}
+      >
+        <form>
+          <Typography
+            textAlign={"left"}
+            variant="h6"
+            component="h2"
+            color={"#339465"}
+          >
+            Contact Us
+          </Typography>
+          <Stack direction={"column"} spacing={2}>
+            <TextField type="text" name="Name" label="Name" required />
+            <TextField type="email" name="Email" label="Email" required />
+            <TextField
+              type="text"
+              name="Mesasge"
+              label="Message"
+              multiline
+              rows={8}
+              required
+            />
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: "#339465",
+                borderRadius: "0px",
+                width: "10px",
+                alignSelf: "center",
+              }}
+              type="submit"
             >
-              Contact Us
-            </Typography>
-            <Stack direction={"column"} spacing={2}>
-              <TextField type="text" name="Name" label="Name" required />
-              <TextField type="email" name="Email" label="Email" required />
-              <TextField
-                type="text"
-                name="mesasge"
-                label="Message"
-                multiline
-                rows={8}
-                required
-              />
-              <Button
-                variant="contained"
-                sx={{
-                  backgroundColor: "#339465",
-                  borderRadius: "0px",
-                  width: "10px",
-                  alignSelf: "center",
-                }}
-                type="submit"
-              >
-                Send
-              </Button>
-            </Stack>
-            <Stack spacing={3} marginTop={"40px"}>
-              <Typography
-                textAlign={"center"}
-                color={"#339465"}
-                variant="body1"
-                component={"h3"}
-              >
-                PLEASE NOTE KENT FILM FOUNDATION HAS NO FULL TIME STAFF SO
-                PLEASE BE PATIENT WITH US.
-              </Typography>
-
-              <Typography textAlign={"center"} variant="body1" component={"h4"}>
-                ALL OUR PROFESSIONAL FILM MENTORS UNDERTAKE AN ENHANCED DBS
-                CHECK
-              </Typography>
-              <Typography
-                textAlign={"center"}
-                color={"#339465"}
-                variant="h6"
-                component={"h5"}
-              >
-                KENT FILM FOUNDATION
-              </Typography>
-              <Typography textAlign={"center"} variant="body1" component={"h6"}>
-                Ramsgate Youth Centre, High St, St Lawrence Ramsgate, CT11 0QG
-              </Typography>
-            </Stack>
-          </form>
-        </Box>
-      ) : (
-        <Box
-          width={"50%"}
-          justifyContent={"center"}
-          alignItems={"center"}
-          margin={"auto"}
-        >
-          <form>
+              Send
+            </Button>
+          </Stack>
+          <Stack spacing={3} marginTop={"40px"}>
             <Typography
-              textAlign={"left"}
-              variant="h6"
-              component="h2"
+              textAlign={"center"}
               color={"#339465"}
+              variant="h6"
+              component={"h3"}
             >
-              Contact Us
+              PLEASE NOTE KENT FILM FOUNDATION HAS NO FULL TIME STAFF SO PLEASE
+              BE PATIENT WITH US.
             </Typography>
-            <Stack direction={"column"} spacing={2}>
-              <TextField type="text" name="Name" label="Name" required />
-              <TextField type="email" name="Email" label="Email" required />
-              <TextField
-                type="text"
-                name="Mesasge"
-                label="Message"
-                multiline
-                rows={8}
-                required
-              />
-              <Button
-                variant="contained"
-                sx={{
-                  backgroundColor: "#339465",
-                  borderRadius: "0px",
-                  width: "10px",
-                  alignSelf: "center",
-                }}
-                type="submit"
-              >
-                Send
-              </Button>
-            </Stack>
-            <Stack spacing={3} marginTop={"40px"}>
-              <Typography
-                textAlign={"center"}
-                color={"#339465"}
-                variant="h6"
-                component={"h3"}
-              >
-                PLEASE NOTE KENT FILM FOUNDATION HAS NO FULL TIME STAFF SO
-                PLEASE BE PATIENT WITH US.
-              </Typography>
-              <Typography textAlign={"center"}>
-                ALL OUR PROFESSIONAL FILM MENTORS UNDERTAKE AN ENHANCED DBS
-                CHECK
-              </Typography>
-              <Typography textAlign={"center"} color={"#339465"}>
-                KENT FILM FOUNDATION
-              </Typography>
-              <Typography textAlign={"center"} marginBottom={4}>
-                Ramsgate Youth Centre, High St, St Lawrence Ramsgate, CT11 0QG
-              </Typography>
-            </Stack>
-          </form>
-        </Box>
-      )}
+            <Typography textAlign={"center"}>
+              ALL OUR PROFESSIONAL FILM MENTORS UNDERTAKE AN ENHANCED DBS CHECK
+            </Typography>
+            <Typography textAlign={"center"} color={"#339465"}>
+              KENT FILM FOUNDATION
+            </Typography>
+            <Typography textAlign={"center"} marginBottom={4}>
+              Ramsgate Youth Centre, High St, St Lawrence Ramsgate, CT11 0QG
+            </Typography>
+          </Stack>
+        </form>
+      </Box>
     </ThemeProvider>
   );
 };
