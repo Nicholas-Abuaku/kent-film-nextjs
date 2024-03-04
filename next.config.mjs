96d5ace0cd8475377ce4a5bec57477cfa55e10d0
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-03-04 11:33:34 +0000

    Will accept images from railway

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "kentfilm.up.railway.app",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
