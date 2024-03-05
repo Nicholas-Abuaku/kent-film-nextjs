165c1e8248ec6a04904258fbfbb59c5bf0b8b31f
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-03-05 15:14:32 +0000

    Changed to use vercel links

--- middleware.ts
+++ middleware.ts
@@ -4,14 +4,12 @@ export default function middleware(req: NextRequest) {
   let verify = req.cookies.get("isLoggedIn");
   let url = req.url;
   if (!verify && url.includes("/dashboard")) {
-    return NextResponse.redirect(
-      "https://kentfilmfoundation.netlify.app/login"
-    );
+    return NextResponse.redirect("https://kentfilmfoundation.vercel.app/login");
   }
 
-  if (verify && url === "https://kentfilmfoundation.netlify.app/login") {
+  if (verify && url === "https://kentfilmfoundation.vercel.app/login") {
     return NextResponse.redirect(
-      `https://kentfilmfoundation.netlify.app/dashboard`
+      `https://kentfilmfoundation.vercel.app/dashboard`
     );
   }
 }
