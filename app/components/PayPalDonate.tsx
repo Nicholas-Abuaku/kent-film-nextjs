c9ab3392224b8b734d1442e784cdf4dc51d48c50
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-03-06 12:30:24 +0000

    Added a URL to paypal donate link

--- app/components/PayPalDonate.tsx
+++ app/components/PayPalDonate.tsx
@@ -2,20 +2,27 @@
 import React from "react";
 import { Button, IconButton } from "@mui/material";
 import PaidIcon from "@mui/icons-material/Paid";
+import Link from "next/link";
 
 const PayPalDonate = () => {
   return (
-    <Button
-      endIcon={<PaidIcon />}
-      variant="contained"
-      sx={{
-        borderRadius: "20px",
-        backgroundColor: "#339465",
-        float: "right",
-      }}
+    <Link
+      href={"https://www.paypal.com/uk/fundraiser/charity/3207559"}
+      target="_blank"
+      rel="noopener noreferrer"
     >
-      Donate
-    </Button>
+      <Button
+        endIcon={<PaidIcon />}
+        variant="contained"
+        sx={{
+          borderRadius: "20px",
+          backgroundColor: "#339465",
+          float: "right",
+        }}
+      >
+        Donate
+      </Button>
+    </Link>
   );
 };
 
