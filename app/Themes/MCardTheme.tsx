12420c7cefbea104a1df2b3da9d0462fecb1c67e
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-03-06 12:28:01 +0000

    using Noto Sans font on MCards for now

--- app/Themes/MCardTheme.tsx
+++ app/Themes/MCardTheme.tsx
@@ -1,24 +1,36 @@
 "use client";
 import { createTheme, responsiveFontSizes } from "@mui/material";
-
+import { Noto_Sans } from "next/font/google";
+import {} from "next/font/google";
+const inter = Noto_Sans({
+  subsets: ["latin"], // Specify the character subsets you need (optional)
+  weight: ["500"], // Include desired font weights
+});
 let MCardTheme = createTheme({
   typography: {
-    fontFamily: "'NunitoSans',Helvetica,Arial,sans-serif",
+    fontFamily: `${inter.style.fontFamily}`,
     subtitle1: {
       fontSize: "1rem",
+      fontWeight: 0,
     },
     h6: {
-      fontSize: "1.2rem",
-      fontWeight: 600, // Set the desired font weight
+      fontSize: "1.25rem",
+      fontWeight: 600,
+    },
+    body1: {
+      fontSize: "1rem",
     },
   },
 });
 MCardTheme = responsiveFontSizes(MCardTheme);
 MCardTheme.typography.h5["@media (max-width:600px)"] = {
-  fontSize: "0.9rem", // Adjust the font size for smaller screens
+  fontSize: "0.9rem", //heading
 };
 MCardTheme.typography.h6["@media (max-width:600px)"] = {
-  fontSize: "0.9rem", // Adjust the font size for smaller screens
+  fontSize: "0.9rem",
+};
+MCardTheme.typography.body1["@media (max-width:600px)"] = {
+  fontSize: "0.7rem",
 };
 
 export default MCardTheme;
