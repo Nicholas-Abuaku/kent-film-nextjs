dfad4872f1a7028636f861ec947787a1fe7375d3
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-03-08 12:13:38 +0000

    Changed page font to noto sans

--- app/Themes/FilmCardTheme.tsx
+++ app/Themes/FilmCardTheme.tsx
@@ -1,13 +1,19 @@
 "use client";
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+import { Noto_Sans } from "next/font/google";
 
+const notoSans = Noto_Sans({
+  subsets: ["latin"],
+  weight: ["200"],
+});
 let FilmCardTheme = createTheme({
   typography: {
     h6: {
-      fontFamily: "'Open Sans', arial, sans-serif",
+      fontFamily: `${notoSans.style.fontFamily}`,
       fontSize: 19,
     },
     subtitle1: {
+      fontFamily: `${notoSans.style.fontFamily}`,
       fontSize: 17,
     },
   },
