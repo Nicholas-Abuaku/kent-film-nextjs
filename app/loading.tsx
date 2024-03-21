0958433374a7d7b3221a29ece2f94afbbaa481ba
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-03-21 11:30:04 +0000

    Added Logo to loading page

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,14 @@
-import { Typography } from "@mui/material";
+import { Grid, Typography, Stack, Skeleton } from "@mui/material";
 import React from "react";
-
+import SolidLogo from "./assets/images/Negative-round-solid.png";
+import Image from "next/image";
 const loading = () => {
   return (
-    <main style={{ textAlign: "center" }}>
-      <Typography variant="h4">Loading...</Typography>
+    <main>
+      <Stack justifyContent={"center"} alignItems={"center"} margin={"auto"}>
+        <Image src={SolidLogo.src} width={256} height={256} alt="Logo" />
+        <Typography variant="h4">Loading Page...</Typography>
+      </Stack>
     </main>
   );
 };
