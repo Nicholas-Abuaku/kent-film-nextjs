4a94a964e8c9951fc653dde57392f355b0097791
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-03-22 11:21:14 +0000

    Removed loading image

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,4 +1,4 @@
-import { Grid, Typography, Stack, Skeleton } from "@mui/material";
+import { Typography, Stack } from "@mui/material";
 import React from "react";
 import SolidLogo from "./assets/images/Negative-round-solid.png";
 import Image from "next/image";
@@ -6,7 +6,6 @@ const loading = () => {
   return (
     <main>
       <Stack justifyContent={"center"} alignItems={"center"} margin={"auto"}>
-        <Image src={SolidLogo.src} width={256} height={256} alt="Logo" />
         <Typography variant="h4">Loading Page...</Typography>
       </Stack>
     </main>
