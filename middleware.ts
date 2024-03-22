cb8f973a0588a61a3e91f2ed6c91126cd3f981ff
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-03-22 20:05:28 +0000

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,10 @@ export default function middleware(req: NextRequest) {
   let verify = req.cookies.get("isLoggedIn");
   let url = req.url;
   if (!verify && url.includes("/dashboard")) {
-    return NextResponse.redirect("https://kentfilmfoundation.vercel.app/login");
+    return NextResponse.redirect("https://kentfilmfoundation.co.uk/login");
   }
 
-  if (verify && url === "https://kentfilmfoundation.vercel.app/login") {
-    return NextResponse.redirect(
-      `https://kentfilmfoundation.vercel.app/dashboard`
-    );
+  if (verify && url === "https://kentfilmfoundation.co.uk/login") {
+    return NextResponse.redirect(`https://kentfilmfoundation.co.uk/dashboard`);
   }
 }
