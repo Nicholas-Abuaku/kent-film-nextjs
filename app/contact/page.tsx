87bf4bda6cce9e2bbafcf58a73ff89ede94e85e7
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-03-22 20:13:26 +0000

    added meta data

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,6 +6,16 @@ const ContactForm = dynamic(() => import("../components/ContactForm"), {
 import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Contact Us",
+  description: "Get in touch with the Kent Film Foundation! ",
+  openGraph: {
+    title: "Contact Us",
+    description: "Get in touch with the Kent Film Foundation! ",
+    type: "website",
+    url: "https://kentfilmfoundation.co.uk/contact",
+  },
+  alternates: {
+    canonical: "https://kentfilmfoundation.co.uk/contact",
+  },
 };
 function Contact() {
   return <ContactForm />;
