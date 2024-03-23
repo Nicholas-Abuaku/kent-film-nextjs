bf3d95f8adff816b11e30f980dfa421f9b1439d0
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-03-23 08:55:31 +0000

    no longer clickable

--- app/components/MCard.tsx
+++ app/components/MCard.tsx
@@ -35,21 +35,21 @@ const MCard = (props: MCardProps) => {
   console.log(props.time);
   return (
     <ThemeProvider theme={createTheme(MCardTheme)}>
-      <Link href={props.link} target="_blank" rel="noopener noreferrer">
-        <CardActionArea>
-          {isMobile ? (
-            <Card
-              sx={{
-                width: "35vw",
-                height: "29vh",
-                display: "flex",
+      {/* <Link href={props.link} target="_blank" rel="noopener noreferrer"> */}
+      {/* <CardActionArea> */}
+      {isMobile ? (
+        <Card
+          sx={{
+            width: "35vw",
+            height: "29vh",
+            display: "flex",
 
-                backgroundColor: "#F0F0ED ",
-                color: "black",
-              }}
-              elevation={10}
-            >
-              {/* <Image
+            backgroundColor: "#F0F0ED ",
+            color: "black",
+          }}
+          elevation={10}
+        >
+          {/* <Image
                 src={props.img}
                 height={0}
                 width={0}
@@ -63,92 +63,88 @@ const MCard = (props: MCardProps) => {
                   marginRight: "5px",
                 }}
               /> */}
-              {/* <Skeleton variant='rectangular' width={400} height={274.05}/> */}
-              <Box
-                sx={{
-                  wordWrap: "break-word",
-                  overflowY: "scroll",
-                }}
+          {/* <Skeleton variant='rectangular' width={400} height={274.05}/> */}
+          <Box
+            sx={{
+              wordWrap: "break-word",
+              overflowY: "scroll",
+            }}
+          >
+            <Stack direction={"row"} spacing={2}>
+              <Typography
+                fontWeight={"bold"}
+                variant="h5"
+                component="h3"
+                // fontFamily={"Open Sans, arial, sans-serif"}
               >
-                <Stack direction={"row"} spacing={2}>
-                  <Typography
-                    fontWeight={"bold"}
-                    variant="h5"
-                    component="h3"
-                    // fontFamily={"Open Sans, arial, sans-serif"}
-                  >
-                    {props.title}
-                  </Typography>
-                </Stack>
+                {props.title}
+              </Typography>
+            </Stack>
 
-                <Typography fontWeight={"bold"} variant="h6" component={"h4"}>
-                  {props.date + " " + props.time}
-                </Typography>
+            <Typography fontWeight={"bold"} variant="h6" component={"h4"}>
+              {props.date + " " + props.time}
+            </Typography>
 
-                <Typography sx={{ marginTop: "10px", marginLeft: "1px" }}>
-                  {props.description}
-                </Typography>
-              </Box>
-            </Card>
-          ) : (
-            <Card
-              sx={{
-                width: "29.59vw",
-                height: "300px",
-                // height: "29vh",
-                display: "flex",
-                backgroundColor: "#F0F0ED ",
-                color: "black",
-              }}
-              elevation={10}
-            >
-              <Image
-                src={props.img}
-                alt={props.title + " image"}
-                title={props.title}
-                width={0}
-                height={0}
-                sizes="100vw"
-                style={{
-                  width: "200px",
-                  height: "auto",
-                  objectFit: "fill",
-                  marginRight: "5px",
-                  borderRadius: "5px",
-                }}
-                loading="lazy"
-              />
+            <Typography sx={{ marginTop: "10px", marginLeft: "1px" }}>
+              {props.description}
+            </Typography>
+          </Box>
+        </Card>
+      ) : (
+        <Card
+          sx={{
+            width: "29.59vw",
+            height: "300px",
+            // height: "29vh",
+            display: "flex",
+            backgroundColor: "#F0F0ED ",
+            color: "black",
+          }}
+          elevation={10}
+        >
+          <Image
+            src={props.img}
+            alt={props.title + " image"}
+            title={props.title}
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{
+              width: "200px",
+              height: "auto",
+              objectFit: "fill",
+              marginRight: "5px",
+              borderRadius: "5px",
+            }}
+            loading="lazy"
+          />
 
-              <Box
-                sx={{
-                  wordWrap: "break-word",
-                  overflow: "auto",
-                  height: "300px",
-                }}
+          <Box
+            sx={{
+              wordWrap: "break-word",
+              overflow: "auto",
+              height: "300px",
+            }}
+          >
+            <Stack direction={"row"} spacing={10}>
+              <Typography
+                fontWeight={"bold"}
+                variant="h6"
+                component="h3"
+                // fontFamily={`${inter.style.fontFamily}, Helvetica, Arial, sans-serif`}
               >
-                <Stack direction={"row"} spacing={10}>
-                  <Typography
-                    fontWeight={"bold"}
-                    variant="h6"
-                    component="h3"
-                    // fontFamily={`${inter.style.fontFamily}, Helvetica, Arial, sans-serif`}
-                  >
-                    {props.title}
-                  </Typography>
-                </Stack>
-                <Typography
-                  fontWeight={"bold"}
-                  variant="subtitle1"
-                  component="h4"
-                >
-                  {props.date} <br /> {props.time}
-                </Typography>
+                {props.title}
+              </Typography>
+            </Stack>
+            <Typography fontWeight={"bold"} variant="subtitle1" component="h4">
+              {props.date} <br /> {props.time}
+            </Typography>
 
-                <Typography sx={{ marginTop: "4px" }}>
-                  {props.description}
-                </Typography>
-                <Link href={props.link}>
-                  {/* <Button
+            <Typography sx={{ marginTop: "4px" }}>
+              {props.description}
+            </Typography>
+            {/* <Link href={props.link}> */}
+            {/* <Button
                     variant="contained"
                     sx={{
                       borderRadius: "70px",
@@ -163,12 +159,12 @@ const MCard = (props: MCardProps) => {
                   >
                     Click here
                   </Button> */}
-                </Link>
-              </Box>
-            </Card>
-          )}
-        </CardActionArea>
-      </Link>
+            {/* </Link> */}
+          </Box>
+        </Card>
+      )}
+      {/* </CardActionArea> */}
+      {/* </Link> */}
     </ThemeProvider>
   );
 };
