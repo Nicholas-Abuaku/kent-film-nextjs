ac87077c1aaa8b01c3b68d6c0e0968c52a69cfc1
nicholas <abuaku1400@gmail.com>
2024-03-28 12:07:14 +0000

    not making eventbrite api call

--- app/api/eventbrite/route.ts
+++ app/api/eventbrite/route.ts
@@ -7,29 +7,29 @@ interface Event {
   status: string; // Assuming "status" is a property with a string value
   // Add other properties as needed, specifying their types
 }
-export async function POST(request: NextRequest, response: NextResponse) {
-  const API_KEY = process.env.NEXT_EVENTBRITE_KEY;
-  const headers = {
-    Authorization: `Bearer ${API_KEY}`,
-    "Content-Type": "application/json",
-  };
+// export async function POST(request: NextRequest, response: NextResponse) {
+//   const API_KEY = process.env.NEXT_EVENTBRITE_KEY;
+//   const headers = {
+//     Authorization: `Bearer ${API_KEY}`,
+//     "Content-Type": "application/json",
+//   };
 
-  try {
-    const response = await axios.get(
-      "https://www.eventbriteapi.com/v3/organizations/335808768129/events/",
-      { headers, params: { status: "live" } }
-    );
+//   try {
+//     const response = await axios.get(
+//       "https://www.eventbriteapi.com/v3/organizations/335808768129/events/",
+//       { headers, params: { status: "live" } }
+//     );
 
-    // Filter events based on "status" property in each event object
-    const liveEvents = response.data.events.filter(
-      (event: Event) => event.status === "live"
-    );
+//     // Filter events based on "status" property in each event object
+//     const liveEvents = response.data.events.filter(
+//       (event: Event) => event.status === "live"
+//     );
 
-    return NextResponse.json(liveEvents); // Return only live events
-  } catch (err: unknown) {
-    console.error(err); // Log detailed error for debugging
-    if (err instanceof Error) {
-      return NextResponse.json({ message: err.message });
-    }
-  }
-}
+//     return NextResponse.json(liveEvents); // Return only live events
+//   } catch (err: unknown) {
+//     console.error(err); // Log detailed error for debugging
+//     if (err instanceof Error) {
+//       return NextResponse.json({ message: err.message });
+//     }
+//   }
+
