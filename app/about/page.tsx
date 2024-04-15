caaf9d38956b4c3729627d340cd7385014fabb0a
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-04-15 11:17:03 +0100

    Rendering new about content component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,11 +3,16 @@ import dynamic from "next/dynamic";
 const AboutContent = dynamic(() => import("../components/AboutContent"), {
   ssr: true,
 });
+import AboutContent2 from "../components/AboutContent2";
 export const metadata: Metadata = {
   title: "About",
 };
 function About() {
-  return <AboutContent />;
+  return (
+    <>
+      <AboutContent2 />
+    </>
+  );
 }
 
 export default About;
