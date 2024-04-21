4bd8fc7617a14df052f79dcbc888c78aa6701b31
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-04-21 10:49:42 +0100

    Revalidate main page 0

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ interface Event {
 const Home = async () => {
   const response = await fetch(
     "https://picayune-belief-production.up.railway.app/api/featured-content",
-    { next: { revalidate: 21600 } }
+    { next: { revalidate: 0 } }
   );
   const fetchEvents = await fetch(
     "https://picayune-belief-production.up.railway.app/api/events",
