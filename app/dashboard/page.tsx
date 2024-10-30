1f7f1e95df68e14d6da7159b7a43219e3bc0fd55
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-10-30 13:38:41 +0000

    Now feteches latest screening data to use as placeholder for edit screening card

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { Grid, Stack, Typography, Paper, Box } from "@mui/material";
 import dynamic from "next/dynamic";
-const ButtonGrid = dynamic(() => import("../components/ButtonGrid"), {
-  ssr: false,
-});
+
 import { Alexandria } from "next/font/google";
 import DashboardScreeningsButton from "../components/DashboardScreeningsButton";
-import ScreeningsPage from "../components/ScreeningsPage";
 import DashboardManageNewsButton from "../components/DashboardManageNewsButton";
 import DashboardManageButton from "../components/DashboardManageFilmClubs";
 import DashboardFeaturedButton from "../components/DashboardFeaturedButton";
@@ -16,7 +13,24 @@ const inter = Alexandria({
   subsets: ["latin"],
   weight: ["500"],
 });
-const page = () => {
+interface LatestScreeningInfo {
+  heading: string;
+  img_Url: string;
+  date: Date;
+  time: string;
+  description: string;
+  eventUrl: string;
+}
+const page = async () => {
+  const response = await fetch(
+    "https://picayune-belief-production.up.railway.app/api/featured-content",
+    { next: { tags: ["latest-screening"] }, cache: "default" }
+  );
+  const latestInfo: LatestScreeningInfo[] = await response.json();
+  let latestImage =
+    "https://picayune-belief-production.up.railway.app/storage/" +
+    latestInfo[0].img_Url;
+
   return (
     <Grid container>
       <Grid
@@ -46,13 +60,24 @@ const page = () => {
       >
         <Grid item xs={8} marginLeft={30} marginRight={5}>
           <Box>
-            <EditScreeningCard />
+            <EditScreeningCard
+              title={latestInfo[0].heading}
+              desc={latestInfo[0].description}
+              date={latestInfo[0].date}
+              image={latestImage}
+            />
           </Box>
         </Grid>
         <Grid item>
           <Stack spacing={4}>
-            <DashboardFeaturedButton />
-            <DashboardFeaturedButton />
+            <DashboardFeaturedButton
+              heading="Add Press Article"
+              linkto="/dashboard/press/new"
+            />
+            <DashboardFeaturedButton
+              heading="Add Screening"
+              linkto="/dashboard/all-screenings/new"
+            />
           </Stack>
         </Grid>
       </Grid>
