34a787b31ccc450ab1c974ae7803ffc84a7e0edf
Nicholas-Abuaku <abuaku1400@gmail.com>
2025-04-05 22:53:06 +0100

    updated to new api

--- app/api/all-events/[id]/route.tsx
+++ app/api/all-events/[id]/route.tsx
@@ -20,7 +20,7 @@ export async function POST(
     console.log("formData:", formData); // Log for debugging
 
     const axiosRes = await axios.post(
-      "https://picayune-belief-production.up.railway.app/api/events/" +
+      "https://kentfilm2025-production.up.railway.app/api/allscreenings/" +
         params.id,
       formData,
       { headers }
