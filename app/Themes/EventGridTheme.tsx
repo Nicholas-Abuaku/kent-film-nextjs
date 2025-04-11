c458cfd07234b7da88e643dabe55f4999b776eb6
Nicholas-Abuaku <abuaku1400@gmail.com>
2025-04-11 21:43:19 +0100

    created theme file for what's on

---
+++ app/Themes/EventGridTheme.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { createTheme, responsiveFontSizes } from "@mui/material";
+import { Noto_Sans } from "next/font/google";
+
+const inter = Noto_Sans({
+  subsets: ["latin"],
+  weight: ["500"],
+});
+let EventGridTheme = createTheme({
+  typography: {
+    
+    fontFamily: `${inter.style.fontFamily}`,
+    subtitle1: {
+      fontSize: "1rem",
+      fontWeight: 400,
+      color:'#1E2A38',
+    },
+    h5: {
+      fontSize: "1.25rem",
+      fontWeight: 500,
+      color:'#1E2A38',
+    },
+    body1: {
+      fontSize: "1rem",
+      color:'white',
+    },
+  },
+});
+EventGridTheme = responsiveFontSizes(EventGridTheme);
+EventGridTheme.typography.h5["@media (max-width:900px)"] = {
+  fontSize: "0.75rem", //heading
+};
+EventGridTheme.typography.h6["@media (max-width:900px)"] = {
+  fontSize: "0.75rem",
+};
+EventGridTheme.typography.body1["@media (max-width:900px)"] = {
+  fontSize: "0.75rem",
+};
+EventGridTheme.typography.subtitle1["@media (max-width:900px)"] = {
+  fontSize: "0.75rem",
+};
+
+export default EventGridTheme;
