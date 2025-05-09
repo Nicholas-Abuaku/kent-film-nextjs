28fe1d4d434c90604c379754a9007a2c1bb622a4
Nicholas-Abuaku <abuaku1400@gmail.com>
2025-05-09 11:48:09 +0100

    removed unused about section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 import dynamic from "next/dynamic";
 import AboutContent2 from "../components/AboutContent2";
-import About3 from "../components/About3";
+
 export const metadata: Metadata = {
   title: "About",
 };
@@ -9,7 +9,7 @@ function About() {
   return (
     <>
       <AboutContent2 />
-      {/* <About3 /> */}
+    
     </>
   );
 }
